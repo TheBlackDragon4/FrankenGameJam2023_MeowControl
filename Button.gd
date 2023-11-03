c3b0ba368d4dec00490cf7c66dc28d3dbdfa3bc2
Alexander Ultsch <83983652+TheBlackDragon4@users.noreply.github.com>
2023-11-04 00:49:20 +0100

    Add scene change from control scene to test scene and back

---
+++ Button.gd
@@ -0,0 +1,25 @@
+extends Button
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+	
+func _input(event):
+	if event.is_pressed():
+		get_tree().change_scene_to_file("res://testScene.tscn")
+	
+#	if event is InputEventMouseButton:
+#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+#			print("Left button clicked", event.position)
+#		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
+#			print("Right button clicked", event.position)
+#		if event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed:
+#			print("Middle button clicked", event.position)
+
+
+
+
