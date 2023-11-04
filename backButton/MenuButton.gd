c7e3a3fca047b36a26647f5eb71556e5af02f93d
Alexander Ultsch <83983652+TheBlackDragon4@users.noreply.github.com>
2023-11-04 03:27:24 +0100

    Changed and simplify the complete button engine

--- backButton/MenuButton.gd
+++ backButton/MenuButton.gd
@@ -10,7 +10,6 @@ func _ready():
 func _process(delta):
 	pass
 
-func _input(event):
-	if event.is_pressed():
-		get_tree().change_scene_to_file("res://controlScene/control_room.tscn")
+func _pressed():
+	get_tree().change_scene_to_file("res://controlScene/control_room.tscn")
 
--- controlScene/Button.gd
+++ controlScene/Button.gd
@@ -8,18 +8,5 @@ func _ready():
 func _process(delta):
 	pass
 	
-func _input(event):
-	if event.is_pressed():
-		get_tree().change_scene_to_file("res://backButton/testScene.tscn")
-	
-#	if event is InputEventMouseButton:
-#		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-#			print("Left button clicked", event.position)
-#		if event.button_index == MOUSE_BUTTON_RIGHT and event.pressed:
-#			print("Right button clicked", event.position)
-#		if event.button_index == MOUSE_BUTTON_MIDDLE and event.pressed:
-#			print("Middle button clicked", event.position)
-
-
-
-
+func _pressed():
+	get_tree().change_scene_to_file("res://backButton/testScene.tscn")
