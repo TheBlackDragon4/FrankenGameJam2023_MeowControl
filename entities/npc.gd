c2363b88734c0f1c2f1ac2c6883f5689438ea741
Funzyy <103183666+Funzyy@users.noreply.github.com>
2023-11-04 01:40:43 +0100

    npc walk

--- entities/npc.gd
+++ entities/npc.gd
@@ -1,9 +1,19 @@
 extends CharacterBody2D
 
+@export var speed = 200
+
+var player
+var distance
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	player = get_tree().root.get_node("Shopfront/Cat")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var to_player = player.global_position - global_position
+	distance = to_player.length()
+	var direction = to_player.normalized()
+	
+	
+	move_and_collide(direction * speed * delta)
