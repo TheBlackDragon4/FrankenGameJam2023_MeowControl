05b8253d0d16b0f704c92c59401bcd9227c46606
David <149827508+davemaster1203@users.noreply.github.com>
2023-11-04 22:19:37 +0100

    Update game_state.gd
    created some variables and arrays

--- game_state.gd
+++ game_state.gd
@@ -2,3 +2,9 @@ extends Node
 
 
 var has_hat = false
+var hat_inventory = [0,0,0] #int array for the hats in the players inventory, one int value defines the ID of the hat
+var hat_catalog = [] #array with indexes for every possible hat in the game
+var karma = 3 #currency for creating hats etc. inital value 3 
+var research_catalog = [] #array with all research informations
+var research_tries = 2 #amount of research scenes a player is allowed to watch per day
+var customer_per_day = 3 #amount of customers per day
