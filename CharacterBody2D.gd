04eb8a9c4828a7c8540441aed0494369c7038cc9
SenseiSapiens <ternes.thomsen@outlook.de>
2023-11-05 00:04:01 +0100

    finished intro

---
+++ CharacterBody2D.gd
@@ -0,0 +1,26 @@
+extends CharacterBody2D
+
+@export var speed = 200
+
+var destination
+var distance
+var direction
+var move = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	destination = get_tree().root.get_node("Intro/Marker_cat")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var to_player = destination.global_position - global_position
+	distance = to_player.length()
+	direction = to_player.normalized()
+	
+	if move == true:
+		if distance > 5:
+			move_and_collide(direction * speed * delta)
+
+
+func _on_timer_timeout():
+	move = true
