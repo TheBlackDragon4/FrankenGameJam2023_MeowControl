abba2ab7c7b2b515fd0b5cd6e8846e167b4d4919
David <149827508+davemaster1203@users.noreply.github.com>
2023-11-04 19:59:46 +0100

    changed moon movement to animation
    changed moon movement to animation
    removed timer and most of the code in the script

--- night_scene.gd
+++ night_scene.gd
@@ -1,9 +1,5 @@
 extends Node
 
-#initial variables for moon offset vector
-var moon_vec_x = 50.0
-var moon_vec_y = -60.0
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -21,33 +17,7 @@ func _on_button_pressed():
 	get_tree().quit()
 
 
-func _on_tree_entered():
-	#executed on entering the scene
-	#set timer intervall and start the timer
-	$Timer.wait_time = 0.1
-	$Timer.start()
-	
-	
 
 
-func _on_timer_timeout():
-	#Timer moves the moon on the sky 
-	
-	#saves the current offset of the moon in var currentposition
-	var currentposition: Vector2 =  $moon.offset
-	
-	#changes the offset vector every intervall so the moons orbit is ecliptic
-	moon_vec_y +=1.5
-	moon_vec_x += 0.5
-	
-	#checks if the moon has not reached its endpoint and if so moves the moon by the vector defined by (moon_vec_x and moon_vec_y)
-	if(currentposition.x <= 5500):
-		$moon.offset = $moon.offset + Vector2 (moon_vec_x,moon_vec_y)
-	
-
-	
-	
-	
-	
 
 
