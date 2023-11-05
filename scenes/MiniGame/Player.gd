09e4d2671d683b10b2916f1b41c5e85055c9dea6
SenseiSapiens <ternes.thomsen@outlook.de>
2023-11-05 14:26:25 +0100

    movement basteln

---
+++ scenes/MiniGame/Player.gd
@@ -0,0 +1,29 @@
+extends CharacterBody2D
+
+const max_speed = 400
+const accel = 1500
+const friction = 600
+
+var input = Vector2.ZERO
+
+func _physics_process(delta):
+	player_movement(delta)
+	
+func get_input():
+	input.x = int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left"))
+	input.y = int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up"))
+	return input.normalized()
+	
+func player_movement(delta):
+	input = get_input()
+	
+	if input == Vector2.ZERO:
+		if velocity.length() > (friction * delta):
+			velocity -= velocity.normalized() * (friction * delta)
+		else:
+			velocity = Vector2.ZERO
+	else:
+		velocity += (input * accel * delta)
+		velocity = velocity.limit_length(max_speed)
+	
+	move_and_slide()
