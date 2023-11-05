21f1d74bcb80574c3777c58061f6b560906aa066
Funzyy <103183666+Funzyy@users.noreply.github.com>
2023-11-05 12:12:26 +0100

    customer fix

--- game_state.gd
+++ game_state.gd
@@ -1,6 +1,7 @@
 extends Node
 
-signal delete_npc
+signal customer_done
+signal start_dialog
 
 var has_hat = false
 var hat_inventory = [null,null,null] #int array for the hats in the players inventory, one int value defines the ID of the hat
@@ -9,3 +10,7 @@ var karma = 3 #currency for creating hats etc. inital value 3
 var research_catalog = [] #array with all research informations
 var research_tries = 2 #amount of research scenes a player is allowed to watch per day
 var customer_per_day = 3 #amount of customers per day
+
+func _process(delta):
+	if Input.is_action_pressed("Escape"):
+		get_tree().quit()
--- scenes/shop/shopfront.gd
+++ scenes/shop/shopfront.gd
@@ -5,20 +5,20 @@ var npc = preload("res://entities/npc.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	GameState.connect("customer_done", _start_timer)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if Input.is_action_pressed("Escape"):
-		get_tree().quit()
+	pass
 
 func _on_timer_timeout():
 	var n = npc.instantiate()
 	n.position = Vector2(-716, 6)
 	$NpcContainer.add_child(n)
-	$Timer.stop()
 	
 	
 func _on_area_2d_body_entered(body):
 	DialogueManager.show_example_dialogue_balloon(load("res://dialog/template.dialogue"), "welcome_customer")
-
+	
+func _start_timer():
+	$Timer.start()
